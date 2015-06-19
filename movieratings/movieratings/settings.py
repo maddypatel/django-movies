0f8fc20c298055256afc50bc9e6a91d7bdbff8c2
Manish Patel <maddypatel@gmail.com>
2015-06-18 22:02:17 -0400

    added

--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'movieratings.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movies',
+        'USER': 'manish',
+        'HOST': '127.0.0.1',
     }
 }
 
